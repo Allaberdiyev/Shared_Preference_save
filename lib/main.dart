550c93aa7b81d69d1e153fb9d4a1ed38bf07b340
Allaberdiyev <149064535+Allaberdiyev@users.noreply.github.com>
2024-06-01 17:47:48 +0500

    SharedPreference

---
+++ lib/main.dart
@@ -0,0 +1,92 @@
+import 'package:flutter/material.dart';
+import 'package:amaliyot/utils/app_constant.dart';
+import 'package:amaliyot/utils/preferences.dart';
+import 'package:amaliyot/view/screens/home_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    super.initState();
+    _initializePreferences();
+  }
+
+  Future<void> _initializePreferences() async {
+    WidgetsFlutterBinding.ensureInitialized();
+    AppConstants.themeMode =
+        await Preferences.getTheme() ? ThemeMode.dark : ThemeMode.light;
+    AppConstants.appBarColor = await Preferences.getAppBarColor();
+    AppConstants.fontSize = await Preferences.getFontSize();
+    AppConstants.textColor = await Preferences.getTextColor();
+    AppConstants.images = await Preferences.getImages();
+    setState(() {});
+  }
+
+  void toggleThemeMode(bool value) async {
+    await Preferences.setTheme(value);
+    AppConstants.themeMode = value ? ThemeMode.dark : ThemeMode.light;
+    setState(() {});
+  }
+
+  void backgroundImage(String imageUrl) async {
+    if (imageUrl.trim().isNotEmpty) {
+      await Preferences.setImages(imageUrl);
+      AppConstants.images = imageUrl;
+      setState(() {});
+    }
+  }
+
+  void appBarColor(Color appBarStyle) async {
+    await Preferences.setAppBarColor(appBarStyle);
+    AppConstants.appBarColor = appBarStyle;
+    setState(() {});
+  }
+
+  void fontSize(double textFontSize) async {
+    await Preferences.setFontSize(textFontSize);
+    AppConstants.fontSize = textFontSize;
+    setState(() {});
+  }
+
+  void textColor(Color textFontColor) async {
+    await Preferences.setTextColor(textFontColor);
+    AppConstants.textColor = textFontColor;
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomeScreen(
+        onThemeChanged: toggleThemeMode,
+        onAppBarColorChanged: appBarColor,
+        onTextFontSize: fontSize,
+        onTextColor: textColor,
+      ),
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        appBarTheme: AppBarTheme(
+          backgroundColor: AppConstants.appBarColor,
+        ),
+        textTheme: TextTheme(
+          bodyMedium: TextStyle(
+            fontSize: AppConstants.fontSize,
+            color: AppConstants.textColor,
+          ),
+        ),
+      ),
+      darkTheme: ThemeData.dark(),
+      themeMode: AppConstants.themeMode,
+    );
+  }
+}
---
+++ lib/utils/app_constant.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AppConstants {
+  static ThemeMode themeMode = ThemeMode.light;
+  static Color appBarColor = Color.fromARGB(255, 6, 180, 0);
+  static double fontSize = 16.0;
+  static Color textColor = Colors.black;
+  static String images = "";
+
+  static const String themeModeKey = "theme_mode";
+  static const String appBarColorKey = "app_bar_color";
+  static const String fontSizeKey = "font_size";
+  static const String textColorKey = "text_color";
+  static const String imagesKey = "images";
+}
